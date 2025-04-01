3e271bf95971097ce7010c798261d8a7f32f381b
Chris Elston <chris.elston@blueyonder.co.uk>
2025-04-01 23:06:17 +0100

    Add pseudo-random number generator

---
+++ api/src/utils/pRandom.ts
@@ -0,0 +1,12 @@
+// an implementation of the splitmix32 Pseudo Random Number Generator
+export const pRandom = (a: number) => {
+  return () => {
+    a |= 0;
+    a = (a + 0x9e3779b9) | 0;
+    let t = a ^ (a >>> 16);
+    t = Math.imul(t, 0x21f0aaad);
+    t = t ^ (t >>> 15);
+    t = Math.imul(t, 0x735a2d97);
+    return ((t = t ^ (t >>> 15)) >>> 0) / 4294967296;
+  };
+};
