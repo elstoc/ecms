c965a7d885930f3f488437f7e0ccee8dc8cc33a0
Chris Elston <chris.elston@blueyonder.co.uk>
2025-04-06 14:42:22 +0100

    VideoToolbox: Add some icon spacing

--- ui/src/videodb/components/VideoToolbox.tsx
+++ ui/src/videodb/components/VideoToolbox.tsx
@@ -4,10 +4,11 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useGetUserIsAdmin } from '@/auth/hooks/useAuthQueries';
 import { Icon } from '@/shared/components/icon';
 import { Toolbox } from '@/shared/components/layout';
-import { useIsDualPanel } from '@/shared/hooks';
 
 import { VideoDbStateContext } from '../hooks/useVideoDbStateContext';
 import { downloadVideoCSV } from '../utils/downloadVideoCSV';
+
+import './VideoToolbox.scss';
 
 export const VideoToolbox = () => {
   const userIsAdmin = useGetUserIsAdmin();
@@ -17,44 +18,50 @@ export const VideoToolbox = () => {
     videoDbState: { apiPath, sortOrder },
     videoDbReducer,
   } = useContext(VideoDbStateContext);
-  const isDualPanel = useIsDualPanel();
 
   const downloadCSV = useCallback(async () => {
     await downloadVideoCSV(apiPath);
   }, [apiPath]);
 
-  if (!userIsAdmin && isDualPanel) {
-    return <></>;
-  }
-
   return (
-    <Toolbox>
-      <Icon
-        name='add'
-        disabled={!userIsAdmin}
-        onClick={() => navigate(`./add?${searchParams.toString()}`)}
-      />
-      <Icon name='download' disabled={!userIsAdmin} onClick={downloadCSV} />
-      <Icon
-        name='sortAscAlpha'
-        color={sortOrder === 'asc' ? 'black' : 'grey'}
-        onClick={() =>
-          videoDbReducer({
-            action: 'setSortOrder',
-            value: 'asc',
-          })
-        }
-      />
-      <Icon
-        name='shuffle'
-        color={sortOrder === 'shuffle' ? 'black' : 'grey'}
-        onClick={() =>
-          videoDbReducer({
-            action: 'setSortOrder',
-            value: 'shuffle',
-          })
-        }
-      />
-    </Toolbox>
+    <div className='video-toolbox'>
+      <Toolbox>
+        {userIsAdmin && (
+          <>
+            <Icon
+              name='add'
+              disabled={!userIsAdmin}
+              onClick={() => navigate(`./add?${searchParams.toString()}`)}
+            />
+            <Icon
+              className='download-icon'
+              name='download'
+              disabled={!userIsAdmin}
+              onClick={downloadCSV}
+            />
+          </>
+        )}
+        <Icon
+          name='sortAscAlpha'
+          color={sortOrder === 'asc' ? 'black' : 'grey'}
+          onClick={() =>
+            videoDbReducer({
+              action: 'setSortOrder',
+              value: 'asc',
+            })
+          }
+        />
+        <Icon
+          name='shuffle'
+          color={sortOrder === 'shuffle' ? 'black' : 'grey'}
+          onClick={() =>
+            videoDbReducer({
+              action: 'setSortOrder',
+              value: 'shuffle',
+            })
+          }
+        />
+      </Toolbox>
+    </div>
   );
 };
