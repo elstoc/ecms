3846b6d8a2f24f8f356ecb8a904061b9577095eb
Chris Elston <chris.elston@blueyonder.co.uk>
2025-04-15 23:22:04 +0100

    VideoDb: Move all content to content component

--- ui/src/videodb/components/VideoDb.tsx
+++ ui/src/videodb/components/VideoDb.tsx
@@ -1,53 +1,26 @@
-import { Suspense } from 'react';
 import { Route, Routes } from 'react-router';
 
 import { VideoDbMetadata } from '@/contracts/site';
 import { NotFoundPage } from '@/shared/components/NotFoundPage';
-import { ContentWithSidebar } from '@/shared/components/layout';
 import { useTitle } from '@/shared/hooks';
-import { InjectComponentTools } from '@/site/components/HeaderToolbox';
 
 import { VideoDbContext, useVideoDbReducer } from '../hooks/useVideoDb';
 
 import { VideoDbContent } from './VideoDbContent';
-import { VideoFilters } from './VideoFilters';
-import { VideoToolbox } from './VideoToolbox';
 
 export const VideoDb = ({ title, apiPath }: VideoDbMetadata) => {
   const reducerProps = useVideoDbReducer(title, apiPath);
 
   useTitle(title);
 
-  const toolbar = (
-    <Suspense>
-      <VideoToolbox />
-    </Suspense>
-  );
-
-  const filters = (
-    <Suspense>
-      <VideoFilters />
-    </Suspense>
-  );
-
-  const content = (
-    <Suspense>
+  return (
+    <VideoDbContext.Provider value={reducerProps}>
       <Routes>
         <Route path='update/:id' element={<VideoDbContent mode='update' />} />
         <Route path='add' element={<VideoDbContent mode='add' />} />
         <Route path='/' element={<VideoDbContent />} />
         <Route key='*' path='*' element={<NotFoundPage />} />
       </Routes>
-    </Suspense>
-  );
-
-  // suspense is wrapped around routes and page elements separately to stop screen flashing
-  return (
-    <VideoDbContext.Provider value={reducerProps}>
-      <Suspense>
-        <ContentWithSidebar content={content} sidebar={filters} />
-        <InjectComponentTools>{toolbar}</InjectComponentTools>
-      </Suspense>
     </VideoDbContext.Provider>
   );
 };
--- ui/src/videodb/components/VideoDbContent.tsx
+++ ui/src/videodb/components/VideoDbContent.tsx
@@ -1,15 +1,37 @@
+import { Suspense } from 'react';
+
+import { ContentWithSidebar } from '@/shared/components/layout';
+import { InjectComponentTools } from '@/site/components/HeaderToolbox';
+
 import { AddOrUpdateVideo } from './AddOrUpdateVideo';
+import { VideoFilters } from './VideoFilters';
 import { VideoList } from './VideoList';
+import { VideoToolbox } from './VideoToolbox';
 
 import './VideoDbContent.scss';
 
 type VideoDbContentProps = { mode?: 'update' | 'add' };
 
 export const VideoDbContent = ({ mode }: VideoDbContentProps) => {
+  const content = (
+    <Suspense>
+      <VideoList />
+    </Suspense>
+  );
+
+  const sidebar = (
+    <Suspense>
+      <VideoFilters />
+    </Suspense>
+  );
+
   return (
     <div className='video-content'>
+      <InjectComponentTools>
+        <VideoToolbox />
+      </InjectComponentTools>
+      <ContentWithSidebar content={content} sidebar={sidebar} />
       <AddOrUpdateVideo mode={mode} />
-      <VideoList />
     </div>
   );
 };
