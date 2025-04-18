580a67241fbf612552501c5a91e8bc54eb8f1ce5
Chris Elston <chris.elston@blueyonder.co.uk>
2025-04-18 23:22:21 +0100

    Updated watched when setting media_watched to 'Y'
    Resolves #29

--- ui/src/videodb/hooks/useEditVideoReducer.ts
+++ ui/src/videodb/hooks/useEditVideoReducer.ts
@@ -4,6 +4,13 @@ import { Video } from '@/contracts/videodb';
 import { KeyValueOfType } from '@/utils';
 
 const videoReducer: (state: Video, action: KeyValueOfType<Video>) => Video = (state, action) => {
+  if (action.key === 'primary_media_watched' && action.value === 'Y') {
+    return {
+      ...state,
+      primary_media_watched: 'Y',
+      watched: 'Y',
+    };
+  }
   return { ...state, [action.key]: action.value };
 };
 
