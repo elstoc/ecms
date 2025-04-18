75d603f21302e4137400152290087a746d808d3c
Chris Elston <chris.elston@blueyonder.co.uk>
2025-04-19 00:02:54 +0100

    Improve useTitle
    Resolves #44

--- ui/src/shared/hooks/useTitle.ts
+++ ui/src/shared/hooks/useTitle.ts
@@ -1,7 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const useTitle = (title: string) => {
+  const originalTitle = useRef(document.title);
+
   useEffect(() => {
     document.title = title;
+
+    return () => {
+      document.title = originalTitle.current;
+    };
   }, [title]);
 };
