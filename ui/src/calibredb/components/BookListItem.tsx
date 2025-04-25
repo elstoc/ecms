33475fc1c4083d436f1682a6d1da1309572818be
Chris Elston <chris.elston@blueyonder.co.uk>
2025-04-26 00:14:39 +0100

    CalibreDb: Initial ListItem formatting
    Resolves #111

--- ui/src/calibredb/components/BookListItem.tsx
+++ ui/src/calibredb/components/BookListItem.tsx
@@ -1,16 +1,19 @@
 /* eslint-disable react/display-name */
-import { Card } from '@blueprintjs/core';
-import { forwardRef } from 'react';
+import { Card, Collapse } from '@blueprintjs/core';
+import { forwardRef, useState } from 'react';
 
 import { Book } from '@/contracts/calibredb';
 
 import { useLookup, useLookupValue } from '../hooks/useCalibreDbQueries';
+
+import './BookListItem.scss';
 
 type BookListItemProps = {
   book: Book;
 };
 
 export const BookListItem = forwardRef<HTMLDivElement, BookListItemProps>(({ book }, ref) => {
+  const [expanded, setExpanded] = useState(false);
   const authors = useLookup('authors');
   const collections = useLookup('collections');
   const format = useLookupValue('formats', book.format);
@@ -20,18 +23,28 @@ export const BookListItem = forwardRef<HTMLDivElement, BookListItemProps>(({ boo
   const tabletStatus = useLookupValue('tabletStatuses', book.tabletStatus);
 
   return (
-    <Card ref={ref}>
-      <div>{book.title}</div>
-      <div>Authors: {book.authors?.map((id) => authors[id]).join(', ')}</div>
-      <div>Rating (quality): {book.rating}</div>
-      <div>Format: {format}</div>
-      <div>Path: {path}</div>
-      <div>Collections: {book.collections?.map((id) => collections[id]).join(', ')}</div>
-      <div>Kobo Status: {koboStatus}</div>
-      <div>Kindle Status: {kindleStatus}</div>
-      <div>Tablet Status: {tabletStatus}</div>
-      <div>Read: {book.read ? 'Yes' : 'No'}</div>
-      <div>Fixed: {book.fixed ? 'Yes' : 'No'}</div>
+    <Card
+      ref={ref}
+      className={`book-list-item ${expanded ? 'expanded' : ''}`}
+      onClick={() => setExpanded((curr) => !curr)}
+    >
+      <div className='primary-info'>
+        <div>
+          <div className='title'>{book.title}</div>
+          <div>{book.authors?.map((id) => authors[id]).join(', ')}</div>
+        </div>
+        <div className='format'>{format}</div>
+      </div>
+      <Collapse isOpen={expanded}>
+        <div>Rating (quality): {book.rating}</div>
+        <div>Path: {path}</div>
+        <div>Collections: {book.collections?.map((id) => collections[id]).join(', ')}</div>
+        <div>Kobo Status: {koboStatus}</div>
+        <div>Kindle Status: {kindleStatus}</div>
+        <div>Tablet Status: {tabletStatus}</div>
+        <div>Read: {book.read ? 'Yes' : 'No'}</div>
+        <div>Fixed: {book.fixed ? 'Yes' : 'No'}</div>
+      </Collapse>
     </Card>
   );
 });
