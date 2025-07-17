d4b7c1d4dcb00cc38fdf662a3ac20b8086c741b3
Chris Elston <chris.elston@blueyonder.co.uk>
2025-07-17 23:23:55 +0100

    Initial Disclosure component (react-aria)

---
+++ ui/src/shared/components/disclosure/Disclosure.tsx
@@ -0,0 +1,84 @@
+import cn from 'classnames';
+import { useEffect, useState } from 'react';
+import {
+  Button as RaButton,
+  ButtonProps as RaButtonProps,
+  Disclosure as RaDisclosure,
+  DisclosurePanel as RaDisclosurePanel,
+  DisclosurePanelProps as RaDisclosurePanelProps,
+  DisclosureProps as RaDisclosureProps,
+  Heading as RaHeading,
+  HeadingProps as RaHeadingProps,
+} from 'react-aria-components';
+
+import './Disclosure.css';
+
+const Button = (props: RaButtonProps) => {
+  return <RaButton {...props} className='ecms-button' />;
+};
+
+const Heading = (props: RaHeadingProps) => {
+  return <RaHeading {...props} className='ecms-heading' />;
+};
+
+export const Disclosure = (props: RaDisclosureProps) => {
+  // The React-Aria Disclosure component cannot be animated in closure because it immediately switches
+  //   the disclosure panel to be visually hidden when its state changes (no matter its height)
+  // This component therefore reports the eventual state (after animation) to the parent and sets a class
+  //   to change the height (which begins the animation) but does not set the contained React-Aria
+  //   expansion state until after the animation has completed
+  const eventualExpansionState = !!props.isExpanded;
+  const setEventualExpansionState = props.onExpandedChange;
+  const [fullExpansionState, setFullExpansionState] = useState(eventualExpansionState);
+
+  // Protect against state misalignment (can happen when fast clicking)
+  // Each time the "full" expansion state is finalised, make sure the "eventual" state matches it
+  useEffect(() => {
+    if (fullExpansionState !== eventualExpansionState) props.onExpandedChange?.(fullExpansionState);
+  }, [fullExpansionState]);
+
+  const handleExpandedChange = async (newExpansionState: boolean) => {
+    setEventualExpansionState?.(newExpansionState);
+
+    // the expansion animation always works (it's immediately visible and can then expand naturally)
+    // the collapse animation needs time to finish (once it's fully hidden it can't continue to collapse)
+    const timeout = newExpansionState ? 0 : 200;
+    setTimeout(() => {
+      setFullExpansionState((state) => !state);
+    }, timeout);
+  };
+
+  const classNames = cn('ecms-disclosure', { visuallyExpanded: eventualExpansionState });
+
+  return (
+    <RaDisclosure
+      {...props}
+      className={classNames}
+      isExpanded={fullExpansionState}
+      onExpandedChange={handleExpandedChange}
+    />
+  );
+};
+
+export const DisclosurePanel = (props: RaDisclosurePanelProps) => {
+  return (
+    <RaDisclosurePanel {...props} className='ecms-disclosure-panel'>
+      <div className='grid-child'>{props.children}</div>
+    </RaDisclosurePanel>
+  );
+};
+
+type TriggerButtonProps = { heading?: string };
+
+export const TriggerButton = ({ heading }: TriggerButtonProps) => {
+  return (
+    <Heading>
+      <Button slot='trigger'>
+        {heading}
+        <svg viewBox='0 0 24 24'>
+          <path d='m8.25 4.5 7.5 7.5-7.5 7.5' />
+        </svg>
+      </Button>
+    </Heading>
+  );
+};
