d70e2dc2e3012078958493c13c5a03736c58a9bb
Chris Elston <chris.elston@blueyonder.co.uk>
2025-07-18 21:47:15 +0100

    Remove missed PasswordInput reference

--- ui/src/shared/components/forms/index.ts
+++ ui/src/shared/components/forms/index.ts
@@ -1,6 +1,5 @@
 export { IntegerInput } from './IntegerInput';
 export { Input } from './Input';
-export { PasswordInput } from './PasswordInput';
 export { SelectDescribedCode } from './SelectDescribedCode';
 export { MultiTagInput } from './MultiTagInput';
 export { SegmentedControlInput } from './SegmentedControlInput';
