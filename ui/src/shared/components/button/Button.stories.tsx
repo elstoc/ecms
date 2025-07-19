7bc4bccabfc93c3560534d53cafe09ea0304db9c
Chris Elston <chris.elston@blueyonder.co.uk>
2025-07-19 12:02:32 +0100

    Button: Add storybook

---
+++ ui/src/shared/components/button/Button.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react-webpack5';
+import { fn } from 'storybook/test';
+
+import { Button } from './Button';
+
+const meta = {
+  title: 'Button',
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'Button text',
+    onClick: () => fn(),
+  },
+};
+
+export const ClearFormatting: Story = {
+  args: {
+    ...Default.args,
+    clearFormatting: true,
+  },
+};
