4e1aa804673348406b03c0f48642256aa4b5b8de
Chris Elston <chris.elston@blueyonder.co.uk>
2025-07-22 23:22:17 +0100

    VideoCard: New UI Component

---
+++ ui/src/shared/components/video-card/VideoCard.stories.tsx
@@ -0,0 +1,67 @@
+import { Meta, StoryObj } from '@storybook/react-webpack5';
+import { useArgs } from 'storybook/internal/preview-api';
+import { fn } from 'storybook/test';
+
+import { VideoCard } from './VideoCard';
+
+const meta = {
+  title: 'VideoCard',
+  component: VideoCard,
+} satisfies Meta<typeof VideoCard>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default = {
+  args: {
+    expanded: false,
+    onExpandedChange: () => undefined,
+    title: 'The Greatest Movie of All Time',
+    formatDesc: '4k Blu Ray',
+    lengthDesc: '180 mins',
+    categoryDesc: 'Movie',
+    locationDesc: 'somewhere',
+    otherMediaDesc: 'Blu Ray (in a box)',
+    tags: ['some', 'tag'],
+    watched: 'P',
+    mediaWatched: 'Y',
+    mediaNotes: 'Going to watch it again',
+    progress: 'Missed the post-credit scene',
+    flagged: false,
+    onFlaggedChange: () => undefined,
+    onEditClick: fn(),
+  },
+  render: (args) => {
+    const [{ expanded, flagged }, updateArgs] = useArgs();
+
+    const onExpandedChange = () => {
+      updateArgs({ expanded: !expanded });
+    };
+
+    const onFlagChange = () => {
+      updateArgs({ flagged: !flagged });
+    };
+
+    return (
+      <VideoCard
+        {...args}
+        onExpandedChange={onExpandedChange}
+        expanded={expanded}
+        onFlaggedChange={onFlagChange}
+        flagged={flagged}
+      />
+    );
+  },
+} satisfies Story;
+
+export const MinimalData = {
+  args: {
+    ...Default.args,
+    tags: undefined,
+    otherMediaDesc: undefined,
+    mediaNotes: undefined,
+    progress: undefined,
+  },
+  render: Default.render,
+} satisfies Story;
---
+++ ui/src/shared/components/video-card/VideoCard.tsx
@@ -0,0 +1,109 @@
+import 'modern-normalize';
+import { forwardRef } from 'react';
+
+// TODO: Get storybook working with @/videodb imports
+import { WatchedIcon } from '../../../videodb/components/WatchedIcon';
+import { Card } from '../card';
+import { Disclosure, DisclosurePanel, TriggerButton } from '../disclosure';
+import { Flag } from '../forms/Flag';
+import { Icon } from '../icon';
+import { Tag, TagListStatic } from '../tag-list-static/TagListStatic';
+
+import './VideoCard.css';
+
+type VideoCardProps = {
+  expanded: boolean;
+  onExpandedChange: (expanded: boolean) => void;
+  title: string;
+  categoryDesc: string;
+  formatDesc: string;
+  lengthDesc: string;
+  locationDesc?: string;
+  otherMediaDesc?: string;
+  tags?: string[];
+  watched: string;
+  mediaWatched: string;
+  flagged?: boolean;
+  onFlaggedChange?: (flagged: boolean) => void;
+  onEditClick?: () => void;
+  mediaNotes?: string;
+  progress?: string;
+};
+
+type TitleAndDataProps = {
+  title: string;
+  data?: string;
+};
+
+export const TitleAndData = ({ title, data }: TitleAndDataProps) => {
+  if (!data) return <></>;
+  return (
+    <div>
+      <strong>{title}:</strong> {data}{' '}
+    </div>
+  );
+};
+
+export const VideoCard = forwardRef<HTMLDivElement, VideoCardProps>(
+  (
+    {
+      expanded,
+      onExpandedChange,
+      title,
+      categoryDesc,
+      formatDesc,
+      lengthDesc,
+      locationDesc,
+      otherMediaDesc,
+      tags,
+      watched,
+      mediaWatched,
+      flagged,
+      onFlaggedChange,
+      onEditClick,
+      mediaNotes,
+      progress,
+    },
+    ref,
+  ) => {
+    return (
+      <Card className='video-card' highlight={expanded} ref={ref}>
+        <Disclosure isExpanded={expanded} onExpandedChange={onExpandedChange}>
+          <div className='info-panel'>
+            <div className='left'>
+              <TriggerButton heading={title} clearButtonFormatting />
+              <div>
+                <WatchedIcon watchedStatus={watched} />
+                <WatchedIcon watchedStatus={mediaWatched} />
+                <span>
+                  {' '}
+                  {formatDesc} ({lengthDesc})
+                </span>
+              </div>
+            </div>
+            <div className='right'>
+              <Flag flagged={flagged} className='priority' onChange={onFlaggedChange} />
+            </div>
+          </div>
+          <DisclosurePanel>
+            <div className='info-panel'>
+              <div className='left'>
+                <TagListStatic label='tags'>
+                  <Tag label={categoryDesc} dark />
+                  {tags?.map((tag) => <Tag key={tag} label={tag} />)}
+                </TagListStatic>
+                <TitleAndData title='Location' data={locationDesc} />
+                <TitleAndData title='Other Media' data={otherMediaDesc} />
+                <TitleAndData title='Media notes' data={mediaNotes} />
+                <TitleAndData title='Progress' data={progress} />
+              </div>
+              <div className='right'>
+                {onEditClick && <Icon label='edit video' icon='edit' onClick={onEditClick} />}
+              </div>
+            </div>
+          </DisclosurePanel>
+        </Disclosure>
+      </Card>
+    );
+  },
+);
