8fee1d1bccec1b7efad44ce94d9749156c13a239
Chris Elston <chris.elston@blueyonder.co.uk>
2025-11-29 23:47:19 +0000

    Prevent manual changing of disclosure expanded state in storybook

--- ui/src/shared/components/book-card/BookCard.stories.tsx
+++ ui/src/shared/components/book-card/BookCard.stories.tsx
@@ -16,6 +16,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default = {
+  argTypes: {
+    expanded: { table: { readonly: true } },
+  },
   args: {
     expanded: false,
     onExpandedChange: () => undefined,
