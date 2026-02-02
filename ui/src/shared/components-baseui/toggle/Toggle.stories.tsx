0627b6b1de5de97971f19f7b1c229bc017f58b44
Chris Elston <chris.elston@blueyonder.co.uk>
2026-02-02 21:18:28 +0000

    Implement BaseUI Toggle

---
+++ ui/src/shared/components-baseui/toggle/Toggle.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react-webpack5';
+import { useArgs } from 'storybook/internal/preview-api';
+
+import { Toggle } from './Toggle';
+
+const meta = {
+  title: 'BaseUI/Toggle',
+  component: Toggle,
+} satisfies Meta<typeof Toggle>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'Toggle text',
+  },
+  render: (args) => {
+    const [{ pressed }, updateArgs] = useArgs();
+
+    const onPressedChange = (pressed: boolean) => {
+      updateArgs({ pressed });
+    };
+
+    return <Toggle {...args} pressed={pressed} onPressedChange={onPressedChange} />;
+  },
+};
---
+++ ui/src/shared/components-baseui/toggle/Toggle.tsx
@@ -0,0 +1,20 @@
+import { Toggle as BaseToggle, ToggleProps as BaseToggleProps } from '@base-ui/react/toggle';
+import cn from 'classnames';
+
+import '../button/Button.css';
+import './Toggle.css';
+
+type ToggleProps = Pick<
+  BaseToggleProps,
+  'id' | 'children' | 'className' | 'pressed' | 'onPressedChange'
+>;
+
+export const Toggle = (props: ToggleProps) => {
+  const className = cn('ec-toggle', 'ec-button', props.className);
+
+  return (
+    <BaseToggle {...props} className={className}>
+      {props.children}
+    </BaseToggle>
+  );
+};
