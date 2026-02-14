e4c7055c5d1910c28b27552ca861e07974818e61
Chris Elston <chris.elston@blueyonder.co.uk>
2026-02-14 11:09:43 +0000

    LabelledField: Take htmlFor prop

--- ui/src/shared/components-baseui/labelled-field/LabelledField.tsx
+++ ui/src/shared/components-baseui/labelled-field/LabelledField.tsx
@@ -7,12 +7,13 @@ type LabelledFieldProps = {
   label: string;
   ariaHideLabel?: boolean;
   children: ReactNode;
+  htmlFor?: string;
 };
 
-export const LabelledField = ({ label, ariaHideLabel, children }: LabelledFieldProps) => {
+export const LabelledField = ({ label, ariaHideLabel, children, htmlFor }: LabelledFieldProps) => {
   return (
     <Field.Root className='ec-labelled-field'>
-      <Field.Label className='ec-field-label' aria-hidden={ariaHideLabel}>
+      <Field.Label className='ec-field-label' aria-hidden={ariaHideLabel} htmlFor={htmlFor}>
         {label}
       </Field.Label>
       {children}
