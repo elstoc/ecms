7d457383408de8694b439393d95711be7cc7be7d
Chris Elston <chris.elston@blueyonder.co.uk>
2026-02-16 19:52:56 +0000

    TagSelect: Re-use existing combobox components/styles

--- ui/src/shared/components-baseui/tag-select/TagSelect.tsx
+++ ui/src/shared/components-baseui/tag-select/TagSelect.tsx
@@ -1,8 +1,11 @@
 import { Combobox } from '@base-ui/react/combobox';
+import cn from 'classnames';
 import * as React from 'react';
 
+import { ComboboxItem, EmptyComboboxItem } from '../combobox/ComboboxItem';
 import { LabelledField } from '../labelled-field';
 
+import comboStyles from '../combobox/Combobox.module.css';
 import styles from './TagSelect.module.css';
 
 export type Item = {
@@ -44,18 +47,18 @@ export const TagSelect = ({ items, label }: TagSelectProps) => {
       </LabelledField>
 
       <Combobox.Portal>
-        <Combobox.Positioner className={styles.Positioner} sideOffset={4} anchor={containerRef}>
-          <Combobox.Popup className={styles.Popup}>
-            <Combobox.Empty className={styles.Empty}>No languages found.</Combobox.Empty>
-            <Combobox.List>
-              {(item: Item) => (
-                <Combobox.Item key={item.value} className={styles.Item} value={item}>
-                  <Combobox.ItemIndicator className={styles.ItemIndicator}>
-                    <CheckIcon className={styles.ItemIndicatorIcon} />
-                  </Combobox.ItemIndicator>
-                  <div className={styles.ItemText}>{item.label}</div>
-                </Combobox.Item>
-              )}
+        <Combobox.Positioner
+          className={comboStyles.Positioner}
+          sideOffset={5}
+          anchor={containerRef}
+        >
+          <Combobox.Popup className={comboStyles.Popup}>
+            <Combobox.Empty className={cn(comboStyles.Empty, comboStyles.List)}>
+              <EmptyComboboxItem emptyMessage='Nothing here' />
+            </Combobox.Empty>
+
+            <Combobox.List className={comboStyles.List}>
+              {(item) => <ComboboxItem key={item.value} item={item} />}
             </Combobox.List>
           </Combobox.Popup>
         </Combobox.Positioner>
@@ -63,14 +66,6 @@ export const TagSelect = ({ items, label }: TagSelectProps) => {
     </Combobox.Root>
   );
 };
-
-function CheckIcon(props: React.ComponentProps<'svg'>) {
-  return (
-    <svg fill='currentcolor' width='10' height='10' viewBox='0 0 10 10' {...props}>
-      <path d='M9.1603 1.12218C9.50684 1.34873 9.60427 1.81354 9.37792 2.16038L5.13603 8.66012C5.01614 8.8438 4.82192 8.96576 4.60451 8.99384C4.3871 9.02194 4.1683 8.95335 4.00574 8.80615L1.24664 6.30769C0.939709 6.02975 0.916013 5.55541 1.19372 5.24822C1.47142 4.94102 1.94536 4.91731 2.2523 5.19524L4.36085 7.10461L8.12299 1.33999C8.34934 0.993152 8.81376 0.895638 9.1603 1.12218Z' />
-    </svg>
-  );
-}
 
 function XIcon(props: React.ComponentProps<'svg'>) {
   return (
