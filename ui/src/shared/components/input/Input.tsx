c2ce6a6ee45d82cc6877b63af68ad981bf44a4ed
Chris Elston <chris.elston@blueyonder.co.uk>
2026-02-17 19:14:04 +0000

    Remove react-aria Input

--- ui/src/shared/components/input/Input.tsx
+++
@@ -1,9 +0,0 @@
-import { Input as RaInput, InputProps as RaInputProps } from 'react-aria-components';
-
-import './Input.css';
-
-type InputProps = RaInputProps;
-
-export const Input = (props: InputProps) => {
-  return <RaInput {...props} className='ecms-input' {...props} />;
-};
