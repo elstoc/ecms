668ecf45a8364dc070a2d4d299741b5ca4ba4b9d
Chris Elston <chris.elston@blueyonder.co.uk>
2026-02-17 22:08:28 +0000

    Implement BaseUI NumberInput
    Resolves #153

---
+++ ui/src/shared/components/number-input/NumberInput.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from '@storybook/react-webpack5';
+import { useArgs } from 'storybook/internal/preview-api';
+
+import { NumberInput } from './NumberInput';
+
+const meta = {
+  title: 'NumberInput',
+  component: NumberInput,
+} satisfies Meta<typeof NumberInput>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: 'Enter a number',
+    value: null,
+    onChange: () => undefined,
+  },
+  render: (args) => {
+    const [{ value }, updateArgs] = useArgs();
+
+    const onChange = (newValue: number | null) => {
+      updateArgs({ value: newValue });
+    };
+
+    return <NumberInput {...args} value={value} onChange={onChange} />;
+  },
+};
+
+export const IntegerOnly: Story = {
+  ...Default,
+  args: { ...Default.args, maximumFractionDigits: 0 },
+};
---
+++ ui/src/shared/components/number-input/NumberInput.tsx
@@ -0,0 +1,32 @@
+import { NumberField as BaseNumberField } from '@base-ui/react/number-field';
+import { useId } from 'react';
+
+import { LabelledField } from '../labelled-field';
+
+import styles from './NumberInput.module.css';
+
+const { Root, Input } = BaseNumberField;
+
+type InputProps = {
+  label: string;
+  value: number | null;
+  onChange: (value: number | null) => void;
+  maximumFractionDigits?: number;
+};
+
+export const NumberInput = ({ label, value, onChange, maximumFractionDigits }: InputProps) => {
+  const id = useId();
+
+  return (
+    <LabelledField label={label} htmlFor={id}>
+      <Root
+        className={styles.Root}
+        value={value}
+        onValueChange={onChange}
+        format={{ maximumFractionDigits }}
+      >
+        <Input className={styles.Input} />
+      </Root>
+    </LabelledField>
+  );
+};
