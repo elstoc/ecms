0e30f14ea4cec8cec699013285b57a79c1013986
Chris Elston <chris.elston@blueyonder.co.uk>
2026-02-17 19:28:02 +0000

    Remove react-aria Label

--- ui/src/shared/components/label/Label.tsx
+++
@@ -1,9 +0,0 @@
-import { Label as RaLabel, LabelProps as RaLabelProps } from 'react-aria-components';
-
-import './Label.css';
-
-type LabelProps = RaLabelProps;
-
-export const Label = (props: LabelProps) => {
-  return <RaLabel className='ecms-label' {...props} />;
-};
