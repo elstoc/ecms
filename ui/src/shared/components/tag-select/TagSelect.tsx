733b895311fbf116d7ccc67282de5f020e8783c1
Chris Elston <chris.elston@blueyonder.co.uk>
2026-02-21 16:48:13 +0000

    TagSelect: Esc should clear query before closing popup
    Resolves #187

--- ui/src/shared/components/tag-select/TagSelect.tsx
+++ ui/src/shared/components/tag-select/TagSelect.tsx
@@ -88,6 +88,11 @@ export const TagSelect = ({
         if (!nextOpen && eventDetails.reason === 'item-press') {
           eventDetails.cancel();
         }
+        /* escape key should clear query before closing popup */
+        if (!nextOpen && eventDetails.reason === 'escape-key' && query) {
+          setQuery('');
+          eventDetails.cancel();
+        }
       }}
     >
       <LabelledField label={label} htmlFor={id}>
