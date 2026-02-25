891d724e30dc74b8e3fc7ec7100b6e639d96a04d
Chris Elston <chris.elston@blueyonder.co.uk>
2026-02-25 21:25:36 +0000

    Use NavMenu for SiteNav
    Resolves #195

--- ui/src/site/components/SiteNav.tsx
+++ ui/src/site/components/SiteNav.tsx
@@ -1,61 +1,39 @@
-import cn from 'classnames';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import { ComponentMetadata, ComponentTypes } from '@/contracts/site';
-import { Classes, Popover } from '@/shared/components-legacy/popover';
+import { NavMenu } from '@/shared/components/nav-menu';
+import { NavItemProps } from '@/shared/components/nav-menu/NavItem';
 
 import { isNavActive } from '../utils/isNavActive';
-
-import './SiteNav.scss';
 
 type SiteNavProps = { siteComponents: ComponentMetadata[] };
 
 export const SiteNav = ({ siteComponents }: SiteNavProps) => {
+  const { pathname } = useLocation();
+
   if (siteComponents.length === 1) {
     return <></>;
   }
 
-  return (
-    <nav className='site-nav'>
-      {siteComponents.map((component) => (
-        <ComponentNavItem key={component.apiPath} component={component} />
-      ))}
-    </nav>
-  );
-};
+  const getLinkItem = (title: string, uiPath: string) => ({
+    title,
+    href: uiPath,
+    active: isNavActive(pathname, uiPath),
+  });
 
-type ComponentNavItemProps = { component: ComponentMetadata };
+  const navItems: NavItemProps[] = siteComponents.map((component) => {
+    if (component.type === ComponentTypes.componentgroup) {
+      return {
+        title: component.title,
+        active: isNavActive(pathname, component.uiPath),
+        subItems: component.components.map((subComponent) =>
+          getLinkItem(subComponent.title, subComponent.uiPath),
+        ),
+      };
+    }
 
-const ComponentNavItem = ({ component }: ComponentNavItemProps) => {
-  const { pathname } = useLocation();
+    return getLinkItem(component.title, component.uiPath);
+  });
 
-  const navTitleClasses = cn('nav-title', { active: isNavActive(pathname, component.uiPath) });
-
-  if (component.type !== ComponentTypes.componentgroup) {
-    return (
-      <Link to={component.uiPath}>
-        <div className={navTitleClasses}>{component.title}</div>
-      </Link>
-    );
-  }
-
-  const subMenuElement = (
-    <div className='sub-menu'>
-      {component.components.map((subComponent) => (
-        <ComponentNavItem key={subComponent.apiPath} component={subComponent} />
-      ))}
-    </div>
-  );
-
-  return (
-    <Popover
-      content={subMenuElement}
-      placement='bottom-start'
-      popoverClassName={Classes.POPOVER_DISMISS}
-      interactionKind='click'
-      minimal={true}
-    >
-      <div className={navTitleClasses}>{component.title}</div>
-    </Popover>
-  );
+  return <NavMenu items={navItems} />;
 };
