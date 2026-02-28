50d898ed16953d782058c1c0088a259b65ef8c8b
Chris Elston <chris.elston@blueyonder.co.uk>
2026-02-28 00:30:31 +0000

    Use new Dialog component for EditVideo
    Resolves #198

--- ui/src/videodb/components/EditVideo.tsx
+++ ui/src/videodb/components/EditVideo.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import { useNavigate, useParams } from 'react-router';
 
 import { useUserIsAdmin } from '@/auth/hooks/useAuthQueries';
-import { Dialog, DialogBody } from '@/shared/components-legacy/dialog';
+import { Dialog } from '@/shared/components/dialog';
 
 import {
   EMPTY_VIDEO_ID,
@@ -36,30 +36,28 @@ export const EditVideo = () => {
   return (
     <Dialog
       title={mode === 'add' ? 'Add Video' : 'Update Video'}
-      isOpen={mode === 'add' || mode === 'update'}
-      onClose={() => navigate(-1)}
-      canEscapeKeyClose={false}
-      canOutsideClickClose={false}
+      open={mode === 'add' || mode === 'update'}
+      onOpenChange={() => navigate(-1)}
+      disablePointerDismissal
+      disableEscapeKeyDismissal
     >
-      <DialogBody useOverflowScrollContainer={false}>
-        <Suspense>
-          {mode === 'add' && (
-            <EditVideoForm
-              key='add'
-              initialVideo={video}
-              onSave={async (newVideo) => postMutate(newVideo, { onSuccess })}
-            />
-          )}
-          {mode === 'update' && (
-            <EditVideoForm
-              key='update'
-              initialVideo={video}
-              onSave={async (updatedVideo) => putMutate({ id, ...updatedVideo }, { onSuccess })}
-              onDelete={async () => deleteMutate(id, { onSuccess })}
-            />
-          )}
-        </Suspense>
-      </DialogBody>
+      <Suspense>
+        {mode === 'add' && (
+          <EditVideoForm
+            key='add'
+            initialVideo={video}
+            onSave={async (newVideo) => postMutate(newVideo, { onSuccess })}
+          />
+        )}
+        {mode === 'update' && (
+          <EditVideoForm
+            key='update'
+            initialVideo={video}
+            onSave={async (updatedVideo) => putMutate({ id, ...updatedVideo }, { onSuccess })}
+            onDelete={async () => deleteMutate(id, { onSuccess })}
+          />
+        )}
+      </Suspense>
     </Dialog>
   );
 };
