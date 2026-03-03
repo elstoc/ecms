87762a2e486cafdebcedb6a6c15fa54c35c7f754
Chris Elston <chris.elston@blueyonder.co.uk>
2026-03-03 13:19:52 +0000

    Rework Toggle/Group
    Resolves #207

--- ui/src/shared/components/toggle/Toggle.tsx
+++ ui/src/shared/components/toggle/Toggle.tsx
@@ -1,7 +1,6 @@
 import { Toggle as BaseToggle, ToggleProps as BaseToggleProps } from '@base-ui/react/toggle';
 import cn from 'classnames';
 
-import buttonStyles from '../../components/button/Button.module.css';
 import styles from './Toggle.module.css';
 
 type ToggleProps = Pick<
@@ -10,7 +9,7 @@ type ToggleProps = Pick<
 >;
 
 export const Toggle = (props: ToggleProps) => {
-  const className = cn(styles.Root, buttonStyles.Root, props.className);
+  const className = cn(styles.Root, props.className);
 
   return (
     <BaseToggle {...props} className={className}>
