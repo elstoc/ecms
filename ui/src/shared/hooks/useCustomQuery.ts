9bf931e0c379dc8264cbd199c1b2a145f9eed845
Chris Elston <chris.elston@blueyonder.co.uk>
2026-03-05 19:35:33 +0000

    Create non-suspense custom query hook

--- ui/src/shared/hooks/useCustomQuery.ts
+++ ui/src/shared/hooks/useCustomQuery.ts
@@ -1,4 +1,4 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQuery, useSuspenseQuery } from '@tanstack/react-query';
 
 import { config } from '@/utils';
 
@@ -12,5 +12,15 @@ type QueryOptions<T> = {
 
 export const useCustomSuspenseQuery = <T>(options: QueryOptions<T>): T => {
   const { data } = useSuspenseQuery({ refetchInterval: config.queryRefetchInterval, ...options });
+  return data;
+};
+
+export const useCustomQuery = <T>(options: QueryOptions<T>): T | undefined => {
+  const { data } = useQuery({
+    refetchInterval: config.queryRefetchInterval,
+    ...options,
+    placeholderData: keepPreviousData,
+  });
+
   return data;
 };
