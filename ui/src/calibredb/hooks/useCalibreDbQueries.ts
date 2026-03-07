b8dfa669bafc88df7a4092b6228025ad4fc2b865
Chris Elston <chris.elston@blueyonder.co.uk>
2026-03-07 10:28:48 +0000

    Remove suspense from calibreDb
    Resolves #223

--- ui/src/calibredb/hooks/useCalibreDbQueries.ts
+++ ui/src/calibredb/hooks/useCalibreDbQueries.ts
@@ -1,7 +1,10 @@
-import { useCustomSuspenseQuery } from '@/shared/hooks';
+import { Book } from '@/contracts/calibredb';
+import { useCustomQuery } from '@/shared/hooks';
 
 import { getCalibreDbBookPaths, getCalibreDbBooks, getCalibreDbLookup } from '../api';
 import { useCalibreDb } from '../hooks/useCalibreDb';
+
+const emptyBooks: Book[] = [];
 
 const useApiPath = () => {
   const {
@@ -15,11 +18,21 @@ export const useBooks = () => {
     state: { apiPath, pages, apiFilters },
   } = useCalibreDb();
 
-  return useCustomSuspenseQuery({
+  const books = useCustomQuery({
     queryKey: ['calibredb', 'books', apiPath, JSON.stringify(apiFilters), pages],
     queryFn: () => getCalibreDbBooks(apiPath, apiFilters, pages),
   });
+
+  return (
+    books ?? {
+      books: emptyBooks,
+      currentPage: 1,
+      totalPages: 1,
+    }
+  );
 };
+
+const emptyBookPaths: Record<string, string> = {};
 
 export const useBookPaths = () => {
   const {
@@ -29,19 +42,25 @@ export const useBookPaths = () => {
     },
   } = useCalibreDb();
 
-  return useCustomSuspenseQuery({
+  const bookPaths = useCustomQuery({
     queryKey: ['calibredb', 'book-paths', apiPath, JSON.stringify(devices)],
     queryFn: () => getCalibreDbBookPaths(apiPath, devices),
   });
+
+  return bookPaths ?? emptyBookPaths;
 };
+
+const emptyLookup: Record<string, string> = {};
 
 export const useLookup = (lookupTable: string) => {
   const apiPath = useApiPath();
 
-  return useCustomSuspenseQuery({
+  const lookup = useCustomQuery({
     queryKey: ['videoDb', 'lookup', apiPath, lookupTable],
     queryFn: () => getCalibreDbLookup(apiPath, lookupTable),
   });
+
+  return lookup ?? emptyLookup;
 };
 
 export const useLookupValue = (lookupTable: string, value?: number) => {
