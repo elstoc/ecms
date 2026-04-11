28ebfc13891f1089ae85bdd08917d40a29d2779a
Chris Elston <chris.elston@blueyonder.co.uk>
2026-04-11 23:27:51 +0100

    CWSDualPanel: Simplify CSS

--- ui/src/site/components/CWSDualPanel.tsx
+++ ui/src/site/components/CWSDualPanel.tsx
@@ -1,10 +1,12 @@
+import cn from 'classnames';
+
 import { ContentWithSideBarProps } from './ContentWithSidebar';
 import { Layout } from './Layout';
 
 export const CWSDualPanel = ({ children, sidebar, componentTools }: ContentWithSideBarProps) => (
   <Layout headerToolsLeft={componentTools}>
-    <div className='cws-container'>
-      <div className={sidebar ? 'cws' : 'cws no-sidebar'}>
+    <div className='cws-root'>
+      <div className={cn('cws', { 'no-sidebar': !sidebar })}>
         {sidebar && <div className='cws-sidebar'>{sidebar}</div>}
         <div className='cws-content'>{children}</div>
       </div>
