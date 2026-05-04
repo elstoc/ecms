4809645dfd9fef6f9bdee4783cf8649469c8f16f
Chris Elston <chris.elston@blueyonder.co.uk>
2026-05-04 12:18:07 +0100

    CWSDualPanel: Minor tweaks

--- ui/src/site/components/CWSDualPanel.tsx
+++ ui/src/site/components/CWSDualPanel.tsx
@@ -10,7 +10,7 @@ import * as styles from './CWSDualPanel.module.scss';
 export const CWSDualPanel = ({ children, sidebar, componentTools }: ContentWithSideBarProps) => (
   <Layout headerToolsLeft={componentTools ? <Toolbar.Root>{componentTools}</Toolbar.Root> : null}>
     <div className={styles.Root}>
-      <div className={cn(styles.CWS, { [styles.NoSidebar]: !sidebar })}>
+      <div className={cn(styles.FixedWidthContent, { [styles.NoSidebar]: !sidebar })}>
         {sidebar && <div className={styles.Sidebar}>{sidebar}</div>}
 
         <div className={styles.Content}>{children}</div>
