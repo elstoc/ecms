639ab78b3d66a5fd1db6d89a91f6490d1bc3d80b
Chris Elston <chris.elston@blueyonder.co.uk>
2026-08-27 22:35:35 +0100

    MusicTutor: Remove some SVG tags

--- ui/src/musictutor/components/Flute.tsx
+++ ui/src/musictutor/components/Flute.tsx
@@ -12,29 +12,26 @@ export const Flute = () => (
       strokeLinejoin='round'
       paintOrder='stroke markers fill'
     >
-      <defs id='defs1' />
-      <g id='layer1'>
-        <ellipse id='L1-Bflat' cx='2.5373795' cy='11.628788' rx='0.68745244' ry='1.0236251' />
-        <ellipse id='L1-B' cx='6.1519299' cy='11.628788' rx='2.2522743' ry='0.98299325' />
-        <circle id='L2-C' cx='7.4556818' cy='7.3943181' r='2' />
-        <circle id='L3-A' cx='13.612689' cy='7.3943181' r='2' />
-        <circle id='L4-G' cx='19.769697' cy='7.3943181' r='2' />
-        <ellipse id='L5-Gsharp' cx='23.695972' cy='3.9358461' rx='2' ry='1.0291389' />
-        <path
-          id='divider'
-          d='m 26.278788,5.1443186 c 0,4.4999988 0,4.4999988 0,4.4999988 v 0 0'
-          strokeWidth={0.1}
-        />
-        <circle id='R2-Fsharp' cx='30.640154' cy='7.3943181' r='2' />
-        <circle id='R3-E' cx='36.797161' cy='7.3943181' r='2' />
-        <circle id='R4-D' cx='42.95417' cy='7.3943181' r='2' />
-        <ellipse id='R5-Dsharp' cx='47.90947' cy='8.1278067' rx='0.90789258' ry='1.2980003' />
-        <ellipse id='R5-LowCsharp' cx='50.796238' cy='8.4097118' rx='1.2811966' ry='0.98460585' />
-        <ellipse id='R5-LowCRoller' cx='50.769833' cy='6.2520604' rx='1.2811966' ry='0.64909506' />
-        <ellipse id='IceLever' cx='27.800188' cy='10.570456' rx='0.68745244' ry='1.0236251' />
-        <ellipse id='DTrill' cx='33.720757' cy='10.86083' rx='0.90789258' ry='1.2980003' />
-        <ellipse id='DsharpTrill' cx='39.873566' cy='10.86083' rx='0.90789258' ry='1.2980003' />
-      </g>
+      <ellipse id='L1-Bflat' cx='2.5373795' cy='11.628788' rx='0.68745244' ry='1.0236251' />
+      <ellipse id='L1-B' cx='6.1519299' cy='11.628788' rx='2.2522743' ry='0.98299325' />
+      <circle id='L2-C' cx='7.4556818' cy='7.3943181' r='2' />
+      <circle id='L3-A' cx='13.612689' cy='7.3943181' r='2' />
+      <circle id='L4-G' cx='19.769697' cy='7.3943181' r='2' />
+      <ellipse id='L5-Gsharp' cx='23.695972' cy='3.9358461' rx='2' ry='1.0291389' />
+      <path
+        id='divider'
+        d='m 26.278788,5.1443186 c 0,4.4999988 0,4.4999988 0,4.4999988 v 0 0'
+        strokeWidth={0.1}
+      />
+      <circle id='R2-Fsharp' cx='30.640154' cy='7.3943181' r='2' />
+      <circle id='R3-E' cx='36.797161' cy='7.3943181' r='2' />
+      <circle id='R4-D' cx='42.95417' cy='7.3943181' r='2' />
+      <ellipse id='R5-Dsharp' cx='47.90947' cy='8.1278067' rx='0.90789258' ry='1.2980003' />
+      <ellipse id='R5-LowCsharp' cx='50.796238' cy='8.4097118' rx='1.2811966' ry='0.98460585' />
+      <ellipse id='R5-LowCRoller' cx='50.769833' cy='6.2520604' rx='1.2811966' ry='0.64909506' />
+      <ellipse id='IceLever' cx='27.800188' cy='10.570456' rx='0.68745244' ry='1.0236251' />
+      <ellipse id='DTrill' cx='33.720757' cy='10.86083' rx='0.90789258' ry='1.2980003' />
+      <ellipse id='DsharpTrill' cx='39.873566' cy='10.86083' rx='0.90789258' ry='1.2980003' />
     </svg>
   </div>
 );
