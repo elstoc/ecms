fc1c8a56eb43e546cdf87bff6d4518d93003ad92
Chris Elston <chris.elston@blueyonder.co.uk>
2026-08-27 22:30:43 +0100

    MusicTutor: Render a flute
    Resolves #288

--- ui/src/musictutor/components/MusicTutor.tsx
+++ ui/src/musictutor/components/MusicTutor.tsx
@@ -1,5 +1,6 @@
 import { ContentWithSidebar } from '@/site/components/ContentWithSidebar';
 
+import { Flute } from './Flute';
 import { StaffRender } from './StaffRender';
 
 const tune = 'L: 1/4\n' + "F,E''";
@@ -8,6 +9,7 @@ export const MusicTutor = () => {
   return (
     <ContentWithSidebar sidebar={null}>
       <StaffRender tune={tune} />
+      <Flute />
     </ContentWithSidebar>
   );
 };
