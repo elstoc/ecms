247bd1dbb9abfcf80fd4ef0b7f978c1b7067de71
Chris Elston <chris.elston@blueyonder.co.uk>
2026-08-28 23:47:37 +0100

    MusicTutor: Create mode selector
    Resolves #289

---
+++ ui/src/musictutor/components/FluteFlashcard.tsx
@@ -0,0 +1,1 @@
+export const FluteFlashcard = () => <div>Placeholder</div>;
--- ui/src/musictutor/components/MusicTutor.tsx
+++ ui/src/musictutor/components/MusicTutor.tsx
@@ -1,16 +1,36 @@
+import { useState } from 'react';
+
+import { Select } from '@/shared/components/select';
 import { ContentWithSidebar } from '@/site/components/ContentWithSidebar';
 
 import { FluteFingeringChart } from './FluteFingeringChart';
+import { FluteFlashcard } from './FluteFlashcard';
 import { PianoKeys } from './PianoKeys';
 
 import * as styles from './MusicTutor.module.css';
 
 export const MusicTutor = () => {
+  const [mode, setMode] = useState('fluteFingering');
+
+  const items = [
+    { value: 'fluteFingering', label: 'Flute Fingering' },
+    { value: 'fluteFlashcard', label: 'Flute Flash Cards' },
+    { value: 'piano', label: 'Piano' },
+  ];
+
   return (
     <ContentWithSidebar sidebar={null}>
       <div className={styles.Root}>
-        <FluteFingeringChart />
-        <PianoKeys />
+        <Select
+          label='Mode'
+          items={items}
+          value={mode}
+          onChange={(newValue) => setMode(newValue || '')}
+          width='lg'
+        />
+        {mode === 'fluteFingering' && <FluteFingeringChart />}
+        {mode === 'fluteFlashcard' && <FluteFlashcard />}
+        {mode === 'piano' && <PianoKeys />}
       </div>
     </ContentWithSidebar>
   );
