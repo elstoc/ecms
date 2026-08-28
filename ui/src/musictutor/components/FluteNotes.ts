bdea66241c7d3ffe919597ed8c6a53497bc04c3c
Chris Elston <chris.elston@blueyonder.co.uk>
2026-08-28 21:09:23 +0100

    MusicTutor: Flute fingering (second octave)

--- ui/src/musictutor/components/FluteNotes.ts
+++ ui/src/musictutor/components/FluteNotes.ts
@@ -38,12 +38,29 @@ export const FLUTE_NOTES_ABC = [
   '_B',
   'B',
   'c',
+  '^c',
+  '_d',
+  'd',
+  '^d',
+  '_e',
+  'e',
+  'f',
+  '^f',
+  '_g',
+  'g',
+  '^g',
+  '_a',
+  'a',
+  '^a',
+  '_b',
+  'b',
+  "c'",
 ];
 
 export const FLUTE_NOTES_ABC_SHARP_ONLY = FLUTE_NOTES_ABC.filter((note) => !note.startsWith('_'));
 export const FLUTE_NOTES_ABC_FLAT_ONLY = FLUTE_NOTES_ABC.filter((note) => !note.startsWith('^'));
 
-export const FLUTE_NOTE_FINGERING: FluteNoteFingering = {
+export const FLUTE_NOTE_FINGERING_MAIN: FluteNoteFingering = {
   C: ['L1-B', 'L2-C', 'L3-A', 'L4-G', 'R2-F', 'R3-E', 'R4-D', 'R5-LowC', 'R5-LowC#'],
   '^C': ['L1-B', 'L2-C', 'L3-A', 'L4-G', 'R2-F', 'R3-E', 'R4-D', 'R5-LowC#'],
   _D: ['L1-B', 'L2-C', 'L3-A', 'L4-G', 'R2-F', 'R3-E', 'R4-D', 'R5-LowC#'],
@@ -62,4 +79,25 @@ export const FLUTE_NOTE_FINGERING: FluteNoteFingering = {
   _B: ['L1-B', 'L2-C', 'R2-F', 'R5-D#'],
   B: ['L1-B', 'L2-C', 'R5-D#'],
   c: ['L2-C', 'R5-D#'],
+  '^c': ['R5-D#'],
+  _d: ['R5-D#'],
+  d: ['L1-B', 'L3-A', 'L4-G', 'R2-F', 'R3-E', 'R4-D'],
+  '^d': ['L1-B', 'L3-A', 'L4-G', 'R2-F', 'R3-E', 'R4-D', 'R5-D#'],
+  _e: ['L1-B', 'L3-A', 'L4-G', 'R2-F', 'R3-E', 'R4-D', 'R5-D#'],
+};
+
+export const FLUTE_NOTE_FINGERING: FluteNoteFingering = {
+  ...FLUTE_NOTE_FINGERING_MAIN,
+  e: FLUTE_NOTE_FINGERING_MAIN['E'],
+  f: FLUTE_NOTE_FINGERING_MAIN['F'],
+  '^f': FLUTE_NOTE_FINGERING_MAIN['^F'],
+  _g: FLUTE_NOTE_FINGERING_MAIN['_G'],
+  g: FLUTE_NOTE_FINGERING_MAIN['G'],
+  '^g': FLUTE_NOTE_FINGERING_MAIN['^G'],
+  _a: FLUTE_NOTE_FINGERING_MAIN['_A'],
+  a: FLUTE_NOTE_FINGERING_MAIN['A'],
+  '^a': FLUTE_NOTE_FINGERING_MAIN['^A'],
+  _b: FLUTE_NOTE_FINGERING_MAIN['_B'],
+  b: FLUTE_NOTE_FINGERING_MAIN['B'],
+  "c'": FLUTE_NOTE_FINGERING_MAIN['c'],
 };
