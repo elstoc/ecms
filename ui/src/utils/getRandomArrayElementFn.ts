a20a2707e637d82bded96c73671b7cd78b8a5975
Chris Elston <chris.elston@blueyonder.co.uk>
2026-08-30 20:26:31 +0100

    Remove console.log

--- ui/src/utils/getRandomArrayElementFn.ts
+++ ui/src/utils/getRandomArrayElementFn.ts
@@ -2,7 +2,6 @@ export const getRandomArrayElementFn = <T>(array: T[], memoryLength: number): ((
   let recentElements: T[] = [];
 
   return () => {
-    console.log(recentElements);
     const filteredArray = array.filter((el) => !recentElements.includes(el));
     const returnValue = filteredArray[Math.floor(Math.random() * filteredArray.length)];
     recentElements = [returnValue, ...recentElements.slice(0, memoryLength - 1)];
